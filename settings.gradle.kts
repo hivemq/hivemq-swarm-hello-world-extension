1a00fdc5f976f36f0fc62a1a411c86e6e7076aab
Silvio Giebl <silvio.giebl@hivemq.com>
2023-08-11 16:26:48 +0200

    Migrate dependencies and plugins to version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,1 @@
 rootProject.name = "hivemq-swarm-hello-world-extension"
-
-pluginManagement {
-    plugins {
-        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
-        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-    }
-}
