458664d2114bb0996befd78040b6d0ad0cbc3df5
David Sondermann <david.sondermann@hivemq.com>
2026-08-06 15:50:42 +0200

    Replace the license plugin with Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     alias(libs.plugins.defaults)
-    alias(libs.plugins.license)
+    alias(libs.plugins.spotless)
 }
 
 group = "com.hivemq.swarm.extensions"
@@ -22,7 +22,8 @@ dependencies {
     implementation(libs.jetbrains.annotations)
 }
 
-license {
-    header = rootDir.resolve("HEADER")
-    mapping("java", "SLASHSTAR_STYLE")
+spotless {
+    java {
+        licenseHeaderFile(rootDir.resolve("HEADER"))
+    }
 }
